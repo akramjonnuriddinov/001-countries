d1bfe620759d6620ac251dbbe929802e69bc92da
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2022-04-12 10:05:47 +0500

    Add dark-mode styles

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@
 const elDarkModeButton = document.querySelector(".dark-mode-btn");
 
 elDarkModeButton.addEventListener('click', function () {
-    document.body.classList.add('dark-mode');
+    document.body.classList.toggle('dark-mode');
 });
