4dd34359b7347f7697791c36982c9080a81731da
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2022-04-12 16:27:18 +0500

    Add details.html and localstorage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,41 @@
 
-const elDarkModeButton = document.querySelector(".dark-mode-btn");
+// const elDarkModeButton = document.querySelector(".dark-mode-btn");
 
-elDarkModeButton.addEventListener('click', function () {
-    document.body.classList.toggle('dark-mode');
+// elDarkModeButton.addEventListener('click', function () {
+//     document.body.classList.toggle('dark-mode');
+    
+// });
+
+
+
+// const check = document.getElementById('chk');
+
+// check.addEventListener('change', () => {
+//   document.body.classList.toggle('dark-mode');
+//   localStorage.darkMode=!localStorage.darkMode;
+// });
+
+// window.onload=function() {
+//   if(localStorage.darkMode) document.body.classList.toggle('dark-mode');
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const theme = localStorage.getItem("theme", "dark");
+    const darkModeToggle = document.getElementById("chk");
+  
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark-mode");
+    } else {
+      document.documentElement.classList.remove("dark-mode");
+    }
+  
+    darkModeToggle.addEventListener("click", function () {
+      document.documentElement.classList.toggle("dark-mode");
+  
+      if (document.documentElement.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+      } else {
+        localStorage.setItem("theme", "light");
+      }
+    });
 });
